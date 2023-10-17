e5335ad520e71aae4a6ea1eaed5a8a310fd37145
AndronakyAndrew <andronakya@gmail.com>
2023-10-17 15:28:09 -0400

    Add project files.

---
+++ Exchange-ReturnApp/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange_ReturnApp
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public decimal Cost { get; set; }
+        public decimal AmountPaid { get; set; }
+        public decimal Change { get; set; }
+        public int SmallCoinsCount { get; set; }
+
+        public Transaction() { }
+
+        public Transaction(decimal cost, decimal amountPaid, decimal change, int smallCoinsCount)
+        {
+            this.Cost = cost;
+            this.AmountPaid = amountPaid;
+            this.Change = change;
+            this.SmallCoinsCount = smallCoinsCount;
+        }
+    }
+}
